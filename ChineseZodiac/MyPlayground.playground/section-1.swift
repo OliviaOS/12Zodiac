c7bc1205a67f632da1f656d9e855e89b6da0567c
OliviaOS <olivia99js@gmail.com>
2016-01-27 17:02:06 -0500

    v1

---
+++ ChineseZodiac/MyPlayground.playground/section-1.swift
@@ -0,0 +1,10 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+
+//logic
+var offset=2008%12
+(2014-offset)%12 //6->ma
+(2015-offset)%12 //7->yang
